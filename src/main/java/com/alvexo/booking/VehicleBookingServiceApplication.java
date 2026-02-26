07e07a5b24b5780846e3b2acc38233941d9a2c7a
san_malar <ksaravananmca@hotmail.com>
2026-02-26 11:10:12 +0530

    Initial commit
    initial project setup commit

---
+++ src/main/java/com/alvexo/booking/VehicleBookingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.alvexo.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleBookingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleBookingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alvexo/booking/VehicleBookingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alvexo.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class VehicleBookingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
