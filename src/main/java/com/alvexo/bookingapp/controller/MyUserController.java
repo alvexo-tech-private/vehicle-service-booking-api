6d2b635490a97abaf7e75878126a990d61945fa8
sreenibalu <153147186+sreenibalu@users.noreply.github.com.>
2026-03-21 18:00:32 +0530

    fix(Return Response) : changes in return response and added description for the endpoint

--- src/main/java/com/alvexo/bookingapp/controller/MyUserController.java
+++ src/main/java/com/alvexo/bookingapp/controller/MyUserController.java
@@ -1,11 +1,12 @@
 package com.alvexo.bookingapp.controller;
 
-import com.alvexo.bookingapp.dto.response.ApiResponse;
+import com.alvexo.bookingapp.dto.response.MyApiResponse;
 import com.alvexo.bookingapp.dto.response.UserResponse;
 import com.alvexo.bookingapp.exception.ResourceNotFoundException;
 import com.alvexo.bookingapp.model.User;
 import com.alvexo.bookingapp.repository.UserRepository;
 import com.alvexo.bookingapp.service.MyUserService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,13 +24,14 @@ public class MyUserController {
     @Autowired
     private MyUserService myUserService;
 
+    @Operation(summary = "Get User Vehicle", description = "Returns paginated list of User Vechicle for the currently authenticated user.")
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<UserResponse>> getUserById(@PathVariable Long id) {
+    public ResponseEntity<MyApiResponse<UserResponse>> getUserById(@PathVariable Long id) {
         User user = userRepository.findById(id)
                 .orElseThrow(() -> new ResourceNotFoundException("User not found"));
 
         UserResponse response = myUserService.convertToResponse(user);
-        return ResponseEntity.ok(ApiResponse.success(response));
+        return ResponseEntity.ok(MyApiResponse.success(response));
     }
 
 }
